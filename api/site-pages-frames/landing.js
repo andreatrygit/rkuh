e10c1834e5b29753d280d80d120d332b3cb9c0c5
Andrea <andreawscotti@gmail.com>
2021-03-28 22:06:24 +0000

    ejs test

---
+++ api/site-pages-frames/landing.js
@@ -0,0 +1,6 @@
+const templateString = '<div class="w-full h-full bg-purple-500 m-1"></div>'
+const resBody = ejs.render(templateString)
+
+module.exports = (req, res) => {
+  res.status(200).body(resBody)
+}
