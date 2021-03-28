d067c846613ff4e0237e264537c8ce44afb4a782
Andrea <andreawscotti@gmail.com>
2021-03-28 23:11:53 +0000

    fix 5

--- api/site-pages-frames/landing.js
+++ api/site-pages-frames/landing.js
@@ -1,5 +1,5 @@
 const ejs = require('ejs');
-const templateString = "<turbo-frame id='/api/site-pages-frames/landing'><div class='w-full h-full bg-purple-500 m-1'></div></turbo-frame>";
+const templateString = "<turbo-frame id='/api/site-pages-frames/landing'><div class='w-full h-4 bg-purple-500 m-1'></div></turbo-frame>";
 const resBody = ejs.render(templateString);
 
 module.exports = (req, res) => {
