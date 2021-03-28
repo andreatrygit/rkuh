abcfac740d4d6aec9638e05eb4f1064dd5ad7e21
Andrea <andreawscotti@gmail.com>
2021-03-28 22:16:04 +0000

    require ejs

--- api/site-pages-frames/landing.js
+++ api/site-pages-frames/landing.js
@@ -1,6 +1,7 @@
-const templateString = '<div class="w-full h-full bg-purple-500 m-1"></div>'
-const resBody = ejs.render(templateString)
+const ejs = require('ejs');
+const templateString = '<div class="w-full h-full bg-purple-500 m-1"></div>';
+const resBody = ejs.render(templateString);
 
 module.exports = (req, res) => {
-  res.status(200).body(resBody)
+  res.status(200).body(resBody);
 }
