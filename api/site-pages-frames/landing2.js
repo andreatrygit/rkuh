6efb5709b8dd631c13cc59fdedacbbb3834beb90
Andrea <andreawscotti@gmail.com>
2021-03-29 22:25:08 +0000

    2 frs test 4

--- api/site-pages-frames/landing2.js
+++ api/site-pages-frames/landing2.js
@@ -1,5 +1,5 @@
 const ejs = require('ejs');
-const templateString = "<turbo-frame id='/api/site-pages-frames/landing2'><div class='w-full h-40 bg-purple-500 m-1'></div></turbo-frame>";
+const templateString = "<turbo-frame id='/api/site-pages-frames/landing2'><div class='w-full h-40 bg-teal-500 m-1'></div></turbo-frame>";
 const resBody = ejs.render(templateString);
 
 module.exports = (req, res) => {
