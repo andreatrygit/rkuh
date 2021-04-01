352f21b682bef4f44a84f27abe71abae810ed2b3
Andrea <andreawscotti@gmail.com>
2021-04-01 22:09:06 +0000

    fade 8

--- api/site-pages-frames/landing.js
+++ api/site-pages-frames/landing.js
@@ -1,5 +1,5 @@
 const ejs = require('ejs');
-const templateString = `<turbo-frame id="/api/site-pages-frames/landing"><div x-init="()=>{$el.classList.add('opacity-100')}" class="w-full h-40 bg-purple-500 opacity-0 transition-opacity duration-1000 m-1"></div></turbo-frame>`;
+const templateString = `<turbo-frame id="/api/site-pages-frames/landing"><div class="w-full h-40 bg-purple-500"></div></turbo-frame>`;
 const resBody = ejs.render(templateString);
 
 module.exports = (req, res) => {
