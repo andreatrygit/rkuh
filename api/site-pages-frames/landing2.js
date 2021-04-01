313685c980cd0a2e8563128509ccbfff49359a5b
Andrea <andreawscotti@gmail.com>
2021-04-01 22:27:08 +0000

    fade 10

--- api/site-pages-frames/landing2.js
+++ api/site-pages-frames/landing2.js
@@ -1,5 +1,5 @@
 const ejs = require('ejs');
-const templateString = "<turbo-frame id='/api/site-pages-frames/landing2'><div class='w-full h-40 bg-pink-500 m-1'></div></turbo-frame>";
+const templateString = "<turbo-frame id='/api/site-pages-frames/landing2'><div class='w-full h-40 bg-pink-500'></div></turbo-frame>";
 const resBody = ejs.render(templateString);
 
 module.exports = (req, res) => {
