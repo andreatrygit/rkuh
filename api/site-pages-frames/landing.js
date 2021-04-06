39b5acbb06697a5150c00d4a3478a12c6fa12f74
Andrea <andreawscotti@gmail.com>
2021-04-06 22:49:05 +0000

    multiline template

--- api/site-pages-frames/landing.js
+++ api/site-pages-frames/landing.js
@@ -1,5 +1,9 @@
 const ejs = require('ejs');
-const templateString = `<turbo-frame id="/api/site-pages-frames/landing"><div class="w-full h-40 bg-purple-500"></div></turbo-frame>`;
+const templateString =
+`<turbo-frame id="/api/site-pages-frames/landing">`+
+    `<div class="w-full h-40 bg-purple-500">`+
+`</div></turbo-frame>`;
+
 const resBody = ejs.render(templateString);
 
 module.exports = (req, res) => {
