ce4b17c5ab9329661c0f200aa1474c98547bdd81
Andrea <andreawscotti@gmail.com>
2021-04-06 22:52:28 +0000

    @click in frames test

--- api/site-pages-frames/landing.js
+++ api/site-pages-frames/landing.js
@@ -1,7 +1,7 @@
 const ejs = require('ejs');
 const templateString =
 `<turbo-frame id="/api/site-pages-frames/landing">`+
-    `<div class="w-full h-40 bg-purple-500">`+
+    `<div @click="console.log('hey')" class="w-full h-40 bg-purple-500">`+
 `</div></turbo-frame>`;
 
 const resBody = ejs.render(templateString);
