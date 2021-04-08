e153bdcd63f99dcddddc0c9397de4302b5a1f567
Andrea <andreawscotti@gmail.com>
2021-04-08 22:23:15 +0000

    isApp test ready 3

--- api/site-pages-frames/landing2.js
+++ api/site-pages-frames/landing2.js
@@ -1,5 +1,9 @@
 const ejs = require('ejs');
-const templateString = "<turbo-frame id='/api/site-pages-frames/landing2'><div class='w-full h-40 bg-pink-500'></div></turbo-frame>";
+const templateString =
+`<turbo-frame id="/api/site-pages-frames/landing2">`+
+    `<div @click="$dispatch('this-is-not-an-app')" class="w-full h-40 bg-pink-500">`+
+`</div></turbo-frame>`;
+
 const resBody = ejs.render(templateString);
 
 module.exports = (req, res) => {
