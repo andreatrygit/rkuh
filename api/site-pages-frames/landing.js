f369266a42dee61235ab9b1cafc0bae257fb9c20
Andrea <andreawscotti@gmail.com>
2021-04-11 21:47:11 +0000

    ++ frames colors

--- api/site-pages-frames/landing.js
+++ api/site-pages-frames/landing.js
@@ -1,7 +1,7 @@
 const ejs = require('ejs');
 const templateString =
 `<turbo-frame id="/api/site-pages-frames/landing">`+
-    `<div @click="$dispatch('this-is-an-app')" class="w-full h-40 bg-purple-500">`+
+    `<div @click="$dispatch('this-is-an-app')" class="w-full h-40 bg-base-200">`+
 `</div></turbo-frame>`;
 
 const resBody = ejs.render(templateString);
