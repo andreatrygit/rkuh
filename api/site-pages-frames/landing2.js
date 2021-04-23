21ab49724bc5d0d5caa42299d0d93d29a83ae75f
Andrea <andreawscotti@gmail.com>
2021-04-23 23:15:52 +0000

    cstf5

--- api/site-pages-frames/landing2.js
+++ api/site-pages-frames/landing2.js
@@ -1,7 +1,7 @@
 const ejs = require('ejs');
 const templateString =
 `<turbo-frame id="/api/site-pages-frames/landing2">`+
-    `<div @click="$dispatch('this-is-not-an-app')" class="w-full h-40 bg-base-200">`+
+    `<div @click="$dispatch('log-out')" class="w-full h-40 bg-base-200">`+
 `</div></turbo-frame>`;
 
 const resBody = ejs.render(templateString);
