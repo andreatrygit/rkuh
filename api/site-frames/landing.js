6726314647b692e86348dfc0c65576d914571dad
Andrea <andreawscotti@gmail.com>
2021-05-04 22:16:37 +0000

    &#39

--- api/site-frames/landing.js
+++ api/site-frames/landing.js
@@ -1,7 +1,7 @@
 const ejs = require('ejs');
 const templateString =
 `<turbo-frame id="/api/site-frames/landing">`+
-    `<button @click="$dispatch('logged-in')" class="w-full btn btn-primary btn-lg">Entra nell&apos&nbspApp<button>`+
+    `<button @click="$dispatch('logged-in')" class="w-full btn btn-primary btn-lg">Entra nell&#39&nbspApp<button>`+
 `</turbo-frame>`;
 
 const resBody = ejs.render(templateString);
