6395bed5bdb9d974712935e88728d376704d431b
Andrea <andreawscotti@gmail.com>
2021-05-23 22:21:09 +0000

    500ms

--- api/app-frames/personal-menu.js
+++ api/app-frames/personal-menu.js
@@ -9,7 +9,7 @@ const menus = {
 }
 const templateString =
 `<turbo-frame id="/api/app-frames/personal-menu">`+
-   `<div class="bg-base-100 flex flex-col justify-center transition-all duration-1000"> `+
+   `<div class="bg-base-100 flex flex-col justify-center transition-all duration-500"> `+
         `<div class="w-full flex flex-row flex-wrap content-center justify-center items-start">`+
                
         `</div>`+
