8c15c2ed0d0c68278e1a3d003a19768de27d23b7
Andrea <andreawscotti@gmail.com>
2021-05-29 10:09:40 +0000

    ky

---
+++ api/rest-api/login/personal/[pin].js
@@ -0,0 +1,12 @@
+module.exports = (req, res) => {
+  if (req.query.pin==="12345")
+  { 
+    res.setHeader('Set-Cookie',["rkuh-session=@@@@@"]);
+    res.status(200).json({"info":"logged-in"});
+  }
+  else
+  {
+    res.setHeader('Set-Cookie',["rkuh-session=''"]);
+    res.status(200).json({"error":"wrong PIN"});
+  }
+}
