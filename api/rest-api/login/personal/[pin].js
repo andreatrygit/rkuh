477cc29b1c273cb546031aec825605bbedbe5eae
andrea <andreawscotti@gmail.com>
2021-06-13 23:27:02 +0200

    test ky timeout

--- api/rest-api/login/personal/[pin].js
+++ api/rest-api/login/personal/[pin].js
@@ -6,7 +6,7 @@ module.exports = (req, res) => {
   }
   else
   {
-    res.setHeader('Set-Cookie',["rkuh-session=''"]);
-    res.status(200).json({"error":"wrong PIN"});
+    setTimeout(()=>res.status(200).json({}),4000)
+    
   }
 }
