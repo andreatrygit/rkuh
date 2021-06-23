70140267c992dd1e4b6fffb3c163f9e4614def19
andrea <andreawscotti@gmail.com>
2021-06-24 00:08:29 +0200

    fix

--- api/site-frames/landing.js
+++ api/site-frames/landing.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 let resBody="";
 
 try {
-  resBody = fs.readFileSync('../../src/templates-html/personal-device-landing.html', 'utf8')
+  resBody = fs.readFileSync('../../../src/templates-html/personal-device-landing.html', 'utf8')
 } catch (err) {
   console.error(err)
 }
