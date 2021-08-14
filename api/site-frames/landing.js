d912792c825bc14291292444ea6172a79c146308
andrea <andreawscotti@gmail.com>
2021-08-14 10:19:16 +0200

    switch on landing based on cookies

--- api/site-frames/landing.js
+++ api/site-frames/landing.js
@@ -1,7 +1,20 @@
 const { readFileSync } = require('fs')
 
-const resBody = readFileSync("src/templates-html/personal-device-landing.html", 'utf8')
-
 module.exports = (req, res) => {
-  res.status(200).send(resBody);
+
+  switch(req.coookies.rkuh-device){
+    case 'timeclock':
+      res.status(200).send(readFileSync("src/templates-html/timeclock-device-landing.html", 'utf8'));
+      break;
+    case 'shared':
+      res.status(200).send(readFileSync("src/templates-html/shared-device-landing.html", 'utf8'));
+      break;
+    case 'personal':
+      res.status(200).send(readFileSync("src/templates-html/personal-device-landing.html", 'utf8'));
+      break;  
+    default:
+      res.status(200).send(readFileSync("src/templates-html/visitor-device-landing.html", 'utf8'));
+      break;  
+  }
+  
 }
