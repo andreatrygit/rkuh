f66235619701ada0efa4d0d7c12ac5e6768b4194
andrea <andreawscotti@gmail.com>
2021-08-20 23:30:07 +0200

    SRC FOLDER REFACTOR

--- api/site-frames/landing.js
+++ api/site-frames/landing.js
@@ -4,16 +4,16 @@ module.exports = (req, res) => {
 
   switch(req.cookies.rkuh_device){
     case 'timeclock':
-      res.status(200).send(readFileSync("src/templates-html/timeclock-device-landing.html", 'utf8'));
+      res.status(200).send(readFileSync("src/lambdas/templates-html/timeclock-device-landing.html", 'utf8'));
       break;
     case 'shared':
-      res.status(200).send(readFileSync("src/templates-html/shared-device-landing.html", 'utf8'));
+      res.status(200).send(readFileSync("src/lambdas/templates-html/shared-device-landing.html", 'utf8'));
       break;
     case 'personal':
-      res.status(200).send(readFileSync("src/templates-html/personal-device-landing.html", 'utf8'));
+      res.status(200).send(readFileSync("src/lambdas/templates-html/personal-device-landing.html", 'utf8'));
       break;  
     default:
-      res.status(200).send(readFileSync("src/templates-html/visitor-device-landing.html", 'utf8'));
+      res.status(200).send(readFileSync("src/lambdas/templates-html/visitor-device-landing.html", 'utf8'));
       break;  
   }
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   mode:'jit',
   purge: {
     enabled: true,
-    content: ['./api/**/*.js','./public/**/*.html','./src/templates-html/**/*.html'],
+    content: ['./api/**/*.js','./public/**/*.html','./src/lambdas/templates-html/**/*.html'],
     options: {
         safelist: [
           /data-theme$/,
