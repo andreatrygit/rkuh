b426902077c6230f5bc03c0cf6aaffff967aa780
andrea <andreawscotti@gmail.com>
2021-09-01 22:57:29 +0200

    test

--- api/rest-api/personal-device-landing/clocking.js
+++ api/rest-api/personal-device-landing/clocking.js
@@ -1,14 +1,5 @@
 import {toughCookie} from '../../../src/lambdas/utils.js';
 
 module.exports = (req, res) => {
-    if (req.query.qrcode==="rkuh-clocking-12345")
-    { 
-      res.setHeader('Set-Cookie',[toughCookie('rkuh_session','',-1)]);
-      res.status(200).json({"info":"clocked correctly"});
-    }
-    else
-    {
-      res.setHeader('Set-Cookie',[toughCookie('rkuh_session','',-1)]);
-      res.status(200).json({"error":"wrong qrCode."});
-    }
+    
   }
