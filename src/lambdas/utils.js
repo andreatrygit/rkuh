7605b35ef85dff941f6e386cc340be3a593b3655
andrea <andreawscotti@gmail.com>
2021-09-06 00:02:59 +0200

    makeTokenPair

--- src/lambdas/utils.js
+++ src/lambdas/utils.js
@@ -3,17 +3,22 @@ export function toughCookie(key,value,maxAge=180){
 }
 
 
-export function makeToken(){
-    const { nanoid } = require('nanoid');
-    const tokenValue = nanoid();
-    const valuetoEncrypt = tokenValue + '###' + process.env.TOKEN_ANTI_FORGERY_KEY_BASE64;
-
-    const { buffer } = require('buffer');
-    const byteKey = buffer.from(process.env.TOKEN_AES256_KEY_BASE64,'base64');
-    const byteIv = buffer.from(process.env.TOKEN_AES256_INIT_VECT_BASE64,'base64');
-
+export function makeTokenPair(){
     const { crypto } = require('crypto');
-    const cipher = crypto.createCipheriv("aes256", byteKey, byteIv);
-    cipher.update(valuetoEncrypt,'utf-8','utf-8');
-    return cipher.final('utf-8');
+    const tokenValueBytes = crypto.randomBytes(16);
+
+    const antiForgeryBytes = Buffer.from(process.env.TOKEN_ANTI_FORGERY_KEY_BASE64,'base64');
+    
+    const valuetoEncrypt = Buffer.concat([tokenValueBytes,antiForgeryBytes]);
+
+    const keyBytes = Buffer.from(process.env.TOKEN_AES256_KEY_BASE64,'base64');
+    const ivBytes = Buffer.from(process.env.TOKEN_AES256_INIT_VECT_BASE64,'base64');
+
+    const cipher = crypto.createCipheriv("aes256",keyBytes, ivBytes);
+    cipher.update(valuetoEncrypt);
+    return (cipher.final('base64'),tokenValueBytes.toString('base64'));
+}
+
+export function redeemToken(token){
+
 }
