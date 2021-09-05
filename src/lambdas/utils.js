b0a2a62ec3d8bbf5b0736a489e3b61c725331c56
andrea <andreawscotti@gmail.com>
2021-09-05 15:37:23 +0200

    CRYPTO START

--- src/lambdas/utils.js
+++ src/lambdas/utils.js
@@ -1,3 +1,19 @@
 export function toughCookie(key,value,maxAge=180){
     return "__Host-" + key + "=" + (value ? value : '') + "; path=/; Secure; HttpOnly; SameSite=Strict; Max-Age=" + maxAge.toString();
 }
+
+
+export function makeToken(){
+    const { nanoid } = require('nanoid');
+    const tokenValue = nanoid();
+    const valuetoEncrypt = tokenValue + '###' + process.env.TOKEN_ANTI_FORGERY_KEY_BASE64;
+
+    const { buffer } = require('buffer');
+    const byteKey = buffer.from(process.env.TOKEN_AES256_KEY_BASE64,'base64');
+    const byteIv = buffer.from(process.env.TOKEN_AES256_INIT_VECT_BASE64,'base64');
+
+    const { crypto } = require('crypto');
+    const cipher = crypto.createCipheriv("aes256", byteKey, byteIv);
+    cipher.update(valuetoEncrypt,'utf-8','utf-8');
+    return cipher.final('utf-8');
+}
