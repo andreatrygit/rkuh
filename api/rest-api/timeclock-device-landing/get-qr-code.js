a9ae16ecfe4165a7b086efc21f8f104742c8b676
andrea <andreawscotti@gmail.com>
2021-09-07 00:49:44 +0200

    better log

--- api/rest-api/timeclock-device-landing/get-qr-code.js
+++ api/rest-api/timeclock-device-landing/get-qr-code.js
@@ -3,7 +3,7 @@ import {makeTokenPair} from '../../../src/lambdas/utils.js';
 module.exports = (req,res) => {
     if (req.cookies['__Host-rkuh_device']==='timeclock') {
         const tokenPair = makeTokenPair();
-        console.log(tokenPair)
+        console.log(tokenPair[0], tokenPair[1])
         res.status(200).json({data: tokenPair[0]});
     } else {
         res.status(200).json({error: 'No Privilege to call this endpoint.'});
