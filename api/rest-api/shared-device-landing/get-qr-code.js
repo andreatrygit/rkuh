09c6cf8a210c2c439f4072d291f94e2bc8f1dde7
andrea <andreawscotti@gmail.com>
2021-09-08 00:35:56 +0200

    shared start

---
+++ api/rest-api/shared-device-landing/get-qr-code.js
@@ -0,0 +1,10 @@
+import {makeTokenPair} from '../../../src/lambdas/utils.js';
+
+module.exports = (req,res) => {
+    if (req.cookies['__Host-rkuh_device']==='shared') {
+        const tokenPair = makeTokenPair();
+        res.status(200).json({data: tokenPair[0]});
+    } else {
+        res.status(200).json({error: 'No Privilege to call this endpoint.'});
+    }
+}
