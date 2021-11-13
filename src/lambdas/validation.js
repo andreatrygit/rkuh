df8dc54d20ee0c5cc8ad75165991b646908048f9
andrea <andreawscotti@gmail.com>
2021-11-13 15:31:59 +0100

    assertions tests ok

--- src/lambdas/validation.js
+++ src/lambdas/validation.js
@@ -1,7 +1,7 @@
-const {isBase64} = require('validator/lib/isEmail');
+var validator = require('validator');
 
 module.exports.isPin=function(pin){
-    typeof(pin)==='string' && /^[0-9]{5}$/.test(pin)
+    return (typeof(pin)==='string' && /^[0-9]{5}$/.test(pin))
 }
 
 module.exports.isPinAssertionsObject = {
@@ -13,15 +13,46 @@ module.exports.isPinAssertionsObject = {
         ["",false,'""'],
         ['aaaaa',false,'aaaaa'],
         ['123456',false,'123456'],
+        ['1234',false,'1234'],
         ['00000',true,'00000'],
         ['12121',true,'12121']
     ]
 }
 
 module.exports.isAppId=function(appid){
-    typeof(appid)==='string' && /^[A-Za-z0-9_-]{21}$/.test(appid) //per nanoid definition
+    return (typeof(appid)==='string' && /^[A-Za-z0-9_-]{21}$/.test(appid)) //per nanoid definition
+}
+
+module.exports.isAppIdAssertionsObject = {
+    functionName:"isAppId",
+    assertions: [
+        [null,false,'null'],
+        [undefined,false,'undefined'],
+        [12345,false,'number 12345'],
+        ["",false,'""'],
+        ['aaaaaaaaaaaaaaaaaaaaaa',false,'aaaaaaaaaaaaaaaaaaaaaa'], //too long
+        ['aaaaaaaaaaaaaaaaaaaa',false,'aaaaaaaaaaaaaaaaaaaa'], //too short
+        ['*********************',false,'*********************'], //wrong chars
+        ['9Wu-dMpZnS2jzEqR_9I1w',true,'9Wu-dMpZnS2jzEqR_9I1w'],
+        ['kWwKxbI-a_8pXHuQUt9Gc',true,'kWwKxbI-a_8pXHuQUt9Gc']
+    ]
 }
 
 module.exports.isToken=function(token){
-    typeof(token)==='string' && token.length>=32 && token.length<=64 && isBase64(token)
+    return (typeof(token)==='string' && token.length>=32 && token.length<=64 && validator.isBase64(token))
+}
+
+module.exports.isTokenAssertionsObject = {
+    functionName:"isToken",
+    assertions: [
+        [null,false,'null'],
+        [undefined,false,'undefined'],
+        [12345,false,'number 12345'],
+        ["",false,'""'],
+        ['aaaaaaaaaaaaaaaaaaaaaa',false,'aaaaaaaaaaaaaaaaaaaaaa'], //too short
+        ['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',false,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'], //too long
+        ['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@',false,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@'], //wrong chars
+        ['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',true,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'],
+        ['YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYQ==',true,'YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYQ==']
+    ]
 }
--- test/test.js
+++ test/test.js
@@ -1,22 +1,37 @@
-const {isPin} = require('../src/lambdas/validation.js')
-const {isPinAssertionsObject} = require('../src/lambdas/validation.js')
+const {isPin, isPinAssertionsObject} = require('../src/lambdas/validation.js')
+const {isAppId, isAppIdAssertionsObject} = require('../src/lambdas/validation.js')
+const {isToken, isTokenAssertionsObject} = require('../src/lambdas/validation.js')
 
 
 function testAssertionsObjects(){
-    console.log('--ASSERTIONS TEST\n')
+    console.log('--ASSERTIONS TEST')
     canditates = [
-        [isPin,isPinAssertionsObject]
+        [isPin,isPinAssertionsObject],
+        [isAppId, isAppIdAssertionsObject],
+        [isToken, isTokenAssertionsObject]
     ]
+    
     canditates.forEach(c => {
-        console.log('\tTesting Function -> ' + c[1].functionName)
+        console.log('\n\tTesting Function -> ' + c[1].functionName + '\n')
         c[1].assertions.forEach(a => {
-            console.log(isPin(''))
+            if (c[0](a[0])===a[1]) {
+                console.log('\t\tOK at ' + a[2])
+            } else {
+                console.log('\t\tFAILS at ' + a[2])
+                throw '\n!!! TESTS STOPPED !!!\n\n'
+            }
         })
     });
+   
 }
 
 module.exports.testAll = function(){
-    console.log('\n**************\n\nSTART OF TESTS\n\n**************\n')
-    testAssertionsObjects();
-
+    try{
+        console.log('\n**************\n\nSTART OF TESTS\n\n**************\n')
+        testAssertionsObjects();
+        console.log('\n**************\n\nEND OF TESTS\n\n**************\n')
+    }
+    catch(e){
+        console.log(e)
+    }
 }
