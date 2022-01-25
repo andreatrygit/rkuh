897a5268885e4d06f724435f19931df4c4e5ab4a
andrea <andreawscotti@gmail.com>
2022-01-26 00:44:11 +0100

    new project layout

--- api/app-frames/forms/any-form.js
+++
@@ -1,7 +0,0 @@
-const { readFileSync } = require('fs')
-
-module.exports = (req, res) => {
-
-    res.status(200).send(readFileSync("src/lambdas/templates-html/forms/any-form.html", 'utf8'));
-   
-}
