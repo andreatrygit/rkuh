650a7bd46f040d07517185197a2282f48eac8aad
andrea <andreawscotti@gmail.com>
2022-02-06 00:03:50 +0100

    TEST START home frame

--- api/frames.js
+++ api/frames.js
@@ -1,6 +1,12 @@
-module.exports = (req, res) => {
-    
+const { readFileSync } = require('fs');
 
-    
-    
+const mapper = {
+    '/':(req,res,payload)=>{
+        res.status(200).send(readFileSync("src/lambdas/templates-html/not-registered/home.html", 'utf8'));
+    }
+}
+
+module.exports = (req, res) => {
+    const {frameName,...payload} = req.body;
+    mapper[frameName](req,res,payload);
 }
