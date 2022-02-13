2fa386680d3599d2c7e3d271874b37ad625e6b28
andrea <andreawscotti@gmail.com>
2022-02-14 00:18:44 +0100

    html to JSON

--- api/frames.js
+++ api/frames.js
@@ -1,8 +1,28 @@
 const { readFileSync } = require('fs');
 
+function htmlStringToJSON(s){
+    return JSON.stringify({htmlString : s});
+}
+
+function filePathToJSON(filePath){
+    return htmlStringToJSON(readFileSync(filePath,'utf8'));
+}
+
+function resStatusWithHtmlFile(status,res,filePath){
+    res.status(status).send(filePathToJSON(filePath));
+}
+
+function _200WithHtmlFile(res,filePath){
+    resStatusWithHtmlFile(200,res,filePath);
+}
+
+function _404WithHtmlFile(res){
+    resStatusWithHtmlFile(404,res,"src/lambdas/templates-html/not-registered/frame-not-found.html");
+}
+
 const mapper = {
     '/':(req,res,payload)=>{
-        res.status(200).send(readFileSync("src/lambdas/templates-html/not-registered/home.html", 'utf8'));
+        _200WithHtmlFile(res,"src/lambdas/templates-html/not-registered/home.html");
     }
 }
 
@@ -13,6 +33,6 @@ module.exports = (req, res) => {
         mapper[frameName](req,res,payload);
     }
     else{
-        res.status(200).send(readFileSync("src/lambdas/templates-html/not-registered/frame-not-found.html", 'utf8')); //soft 404
+        _404WithHtmlFile(res);
     }
 }
