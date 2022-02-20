ba844647a7c8b7df491908eb859f84dccf5919a9
andrea <andreawscotti@gmail.com>
2022-02-20 01:23:29 +0100

    datajs errors

--- api/data.js
+++ api/data.js
@@ -1,3 +1,52 @@
+function dataToResBody(obj){
+  return JSON.stringify({data: obj});
+}
+
+function errorToResBody(errorName,errorMessage,errorData=null){
+  let errorObj = {};
+  errorObj.name = errorName;
+  errorObj.message = errorMessage;
+  errorObj.data = errorData;
+  return JSON.stringify({error:errorObj});
+}
+
+function response(res,obj){
+  res.status(200).send(dataToResBody(obj));
+}
+
+function funcNameError(res){
+  res.status(200).send(errorToResBody('FuncNameError','Function name unknown.'));
+}
+
+function requestError(res,errorMessage){
+  res.status(200).send(errorToResBody('RequestError',errorMessage));
+}
+
+
+function dataValidationError(res,errorData){
+  res.status(200).send(errorToResBody('DataValidationError','Failed data validation.',errorData));
+}
+
+function driverError(res,errorMessage){
+  res.status(200).send(errorToResBody('DriverError',errorMessage));
+}
+
+function sessionError(res){
+  res.status(200).send(errorToResBody('SessionError','Failed to start a session.'));
+}
+
+function transactionError(res,errorMessage){
+  res.status(200).send(errorToResBody('TransactionError',errorMessage));
+}
+
+function privilegeError(res){
+  res.status(200).send(errorToResBody('PrivilegeError','Transaction refuted for insufficient privilege.'));
+}
+
+function semanticValidationError(res){
+  res.status(200).send(errorToResBody('SemanticValidationError','Transaction refuted for failed semantic validation.'));
+}
+
 module.exports = (req, res) => {
     
 
